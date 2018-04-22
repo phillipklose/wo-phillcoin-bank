01925ace6632016b59c336b15e5b8195cffb5984
phillipklose <filip.klose@gmail.com>
2018-04-22 10:25:15 +0200

    Added usermapper test

---
+++ src/test/java/com/filip/klose/wophillcoinbank/mapper/UserMapperTest.java
@@ -0,0 +1,28 @@
+package com.filip.klose.wophillcoinbank.mapper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.modelmapper.ModelMapper;
+
+import com.filip.klose.wophillcoinbank.builder.UserBuilder;
+import com.filip.klose.wophillcoinbank.entity.User;
+import com.filip.klose.wophillcoinbank.model.UserDto;
+
+public class UserMapperTest {
+
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @Test
+    public void testIfConvertedEntityToDtoWorksProperly() {
+        User userEntity = new UserBuilder().setLogin("testLogin").setPassword("password").setFirstName("testFirstName")
+                .setLastName("testLastName").setEmail("testEmail").build();
+
+        UserDto convertedUserEntityToDto = modelMapper.map(userEntity, UserDto.class);
+
+        assertEquals(convertedUserEntityToDto.getLogin(), userEntity.getLogin());
+        assertEquals(convertedUserEntityToDto.getFirstName(), userEntity.getFirstName());
+        assertEquals(convertedUserEntityToDto.getLastName(), userEntity.getLastName());
+        assertEquals(convertedUserEntityToDto.getEmail(), userEntity.getEmail());
+    }
+}
